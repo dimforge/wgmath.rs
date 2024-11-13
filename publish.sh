6cfeb0aca97854a88505a00d57faab2ae753f8e2
Thierry Berger <contact@thierryberger.com>
2024-11-13 17:33:06 +0100

    compress wasm files + some docs on it

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 PUBLISH_MODE=1 yarn build
-cp .htaccess build/.
-rsync -av --delete-after build/ crozet@ssh.cluster003.hosting.ovh.net:/home/crozet/wgmath/
+# cp .htaccess build/.
+# rsync -av --delete-after build/ crozet@ssh.cluster003.hosting.ovh.net:/home/crozet/wgmath/
+
+sudo cp .htaccess build/.
+sudo cp -r build/* /var/www/html/test-wgmath/
+sudo cp .htaccess /var/www/html/test-wgmath/
+
+sudo systemctl restart apache2
