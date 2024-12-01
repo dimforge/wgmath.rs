461f8262c3b44c1bd20b910ec46fe03e117e2835
Sébastien Crozet <sebcrozet@dimforge.com>
2024-12-01 22:24:37 +0100

    feat: add example code injection

---
+++ generate_user_guides.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -v
+
+mkdir -p docs/user_guides/wgcore/
+find docs/user_guides/templates/wgcore -type f -print0 | OUTPUT_FOLDER=docs/user_guides/templates_injected xargs -0 ./inject_code_in_user_guides.sh
+cp -r docs/user_guides/templates_injected/* docs/user_guides/wgcore/.
+rm -rf docs/user_guides/templates_injected
