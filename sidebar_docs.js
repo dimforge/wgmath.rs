f3dda4129ce1af0ef85548150038cd7bed69862b
Sébastien Crozet <sebcrozet@dimforge.com>
2024-12-01 22:24:09 +0100

    feat: add some wgcore documentation

--- sidebar_docs.js
+++ sidebar_docs.js
@@ -1,6 +1,46 @@
+
+let templates = {
+    '<Templates>': [
+        {
+            'WgCore Book': [
+                'user_guides/templates/wgcore/shaders_composition',
+                'user_guides/templates/wgcore/buffers_initialization',
+                'user_guides/templates/wgcore/buffers_readback',
+                'user_guides/templates/wgcore/hot_reloading',
+                'user_guides/templates/wgcore/timestamp_queries',
+                'user_guides/templates/wgcore/overwriting_shaders'
+            ],
+        }
+    ]
+};
+
+let templates_injected = {
+    'WgCore Book': [
+        'user_guides/wgcore/shaders_composition',
+        'user_guides/wgcore/buffers_initialization',
+        'user_guides/wgcore/buffers_readback',
+        'user_guides/wgcore/hot_reloading',
+        'user_guides/wgcore/timestamp_queries',
+        'user_guides/wgcore/overwriting_shaders'
+    ],
+}
+
+let user_guides;
+
+if (!process.env.PUBLISH_MODE) {
+    user_guides = {
+        ...templates,
+        '<templates-injected>': templates_injected
+    };
+} else {
+    user_guides = templates_injected;
+}
+
+
 const config = {
     docs: [
         'about_wgmath',
+        user_guides,
         {
             'WGSL Documentation': [
                 'api/wgblas-api-doc',
